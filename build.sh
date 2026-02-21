be14e21fbe13960f91e1f662581006dd7c7f2fc1
raine-works <raine@raineworks.com>
2026-02-20 22:23:20 -0700

    added build script and package.json

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PACKAGE_NAME=$(jq -r .name package.json)
+VERSION=$(jq -r .version package.json)
+GITHUB_USERNAME="raine-works"
+
+if docker buildx ls | grep -q docker-container; then
+    echo "Using existing builder."
+else
+    docker buildx create --name container --driver=docker-container
+fi
+
+docker buildx build \
+    -f packages/${PACKAGE_NAME}/Dockerfile \
+    --platform linux/amd64,linux/arm64 \
+    -t ghcr.io/${GITHUB_USERNAME}/${PACKAGE_NAME}:latest \
+    -t ghcr.io/${GITHUB_USERNAME}/${PACKAGE_NAME}:${VERSION} \
+    --builder=container \
+    --push \
+    .
